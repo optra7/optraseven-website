0ca5ff1f7b783a26e1894f370dff0fdbe0142b40
Jahangir Alam <jahangir025@gmail.com>
2025-11-18 21:42:48 +0600

    footer and landing page responsive design

--- app/public/wp-content/themes/optraseven-website-theme/footer.php
+++ app/public/wp-content/themes/optraseven-website-theme/footer.php
@@ -170,6 +170,59 @@
                     </div>
                 </div>
 
+                <div class="o7-footer__icon-chip social-media-mobile-view">
+                    <div class="o7-footer__icon-chip-icon-wraper">
+                        <a href="#">
+                            <div class="o7-footer__icon-chip-icon-box">
+                                <svg class="o7-footer__icon-chip-icon" aria-hidden="true" focusable="false">
+                                    <use href=<?=get_template_directory_uri() . "/assets/icons/svg-icon-sprite.svg#facebook"?>></use>
+                                </svg>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="o7-footer__icon-chip-icon-box">
+                                <svg class="o7-footer__icon-chip-icon" aria-hidden="true" focusable="false">
+                                    <use href=<?=get_template_directory_uri() . "/assets/icons/svg-icon-sprite.svg#x-twitter"?>></use>
+                                </svg>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="o7-footer__icon-chip-icon-box">
+                                <svg class="o7-footer__icon-chip-icon" aria-hidden="true" focusable="false">
+                                    <use href=<?=get_template_directory_uri() . "/assets/icons/svg-icon-sprite.svg#instagram"?>></use>
+                                </svg>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="o7-footer__icon-chip-icon-box">
+                                <svg class="o7-footer__icon-chip-icon" aria-hidden="true" focusable="false">
+                                    <use href=<?=get_template_directory_uri() . "/assets/icons/svg-icon-sprite.svg#whatsapp"?>></use>
+                                </svg>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="o7-footer__icon-chip-icon-box">
+                                <svg class="o7-footer__icon-chip-icon" aria-hidden="true" focusable="false">
+                                    <use href=<?=get_template_directory_uri() . "/assets/icons/svg-icon-sprite.svg#linkedin"?>></use>
+                                </svg>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="o7-footer__icon-chip-icon-box">
+                                <svg class="o7-footer__icon-chip-icon" aria-hidden="true" focusable="false">
+                                    <use href=<?=get_template_directory_uri() . "/assets/icons/svg-icon-sprite.svg#github"?>></use>
+                                </svg>
+                            </div>
+                        </a>
+                        <svg class="o7-footer__icon-chip-bg-icon-one" aria-hidden="true" focusable="false">
+                            <use href=<?=get_template_directory_uri() . "/assets/icons/svg-icon-sprite.svg#footer-icon-chip-bg-icon-1"?>></use>
+                        </svg>
+                        <svg class="o7-footer__icon-chip-bg-icon-two" aria-hidden="true" focusable="false">
+                            <use href=<?=get_template_directory_uri() . "/assets/icons/svg-icon-sprite.svg#footer-icon-chip-bg-icon-2"?>></use>
+                        </svg>
+                    </div>
+                </div>
+
                 <div class="o7-footer__copyright-container">
                     <div class="o7-footer__copyright">
                         <span class="o7-footer__copyright--highlight">OptraSeven</span>
