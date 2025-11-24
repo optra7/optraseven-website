9faa75726062b5b55f90fb43b54dac8622a3beac
Jahangir Alam <jahangir025@gmail.com>
2025-11-24 12:40:45 +0600

    responsive menuj fixing

--- app/public/wp-content/themes/optraseven-website-theme/js/shared.js
+++ app/public/wp-content/themes/optraseven-website-theme/js/shared.js
@@ -120,3 +120,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		}, 2500);
 	}
 });
+
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
