bcc86bcb5d01140f31b176b717e2f761db4ef0ef
suhid98 <suhid17@gmail.com>
2025-11-30 11:30:21 +0600

    qoute form fix..

--- app/public/wp-content/themes/optraseven-website-theme/js/quote-form.js
+++ app/public/wp-content/themes/optraseven-website-theme/js/quote-form.js
@@ -1,5 +1,16 @@
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById('website-url').addEventListener('blur', function () {
+        let v = this.value.trim();
+        if (!v) return;
+
+        // If it has no protocol, assume https
+        if (!/^https?:\/\//i.test(v)) {
+            this.value = 'https://' + v;
+        }
+    });
+
     // Initialize datepicker
     const dateInput = document.querySelector(".o7-get-the-quote__input-date");
     if (dateInput) {
