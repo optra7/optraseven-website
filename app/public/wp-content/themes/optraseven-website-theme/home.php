52e39a65af784fe8c8623b5b06f3abb889d1fe79
MursalinShakil <mursalinshakil007@gmail.com>
2026-03-11 14:45:41 +0600

    blog & testimonial fix

--- app/public/wp-content/themes/optraseven-website-theme/home.php
+++ app/public/wp-content/themes/optraseven-website-theme/home.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Universal Archive Template (for filtering)
  */
@@ -38,7 +39,7 @@ wp_localize_script('o7-archive-filter', 'archiveFilterData', [
                                     data-filter="<?php echo esc_attr($term->slug); ?>">
                                     <?php echo esc_html($term->name); ?>
                                 </li>
-                            <?php endforeach;
+                        <?php endforeach;
                         endif;
                         ?>
                     </ul>
@@ -67,85 +68,91 @@ wp_localize_script('o7-archive-filter', 'archiveFilterData', [
                             $featured_img = get_the_post_thumbnail_url($post_id, 'large');
                             $categories = wp_get_post_terms($post_id, $taxonomy, ['fields' => 'slugs']);
                             $category_classes = !empty($categories) ? implode(' ', $categories) : '';
-                            ?>
+                    ?>
                             <article class="o7-list-page-filter__card <?php echo esc_attr($category_classes); ?>" data-category="<?php echo esc_attr($category_classes); ?>">
-                                
-                                    <!-- Featured Image -->
-                                    <div class="o7-list-page-filter__card-image-wrapper o7-card__image-wrapper o7-card__image-wrapper--rounded o7-card__image-wrapper--with-bg">
-                                        <div class="o7-content-card__image-frame o7-card__image-frame">
-                                            <a href="<?php echo get_permalink($post_id); ?>">
-                                                <?php if ($featured_img): ?>
+
+                                <!-- Featured Image -->
+                                <div class="o7-list-page-filter__card-image-wrapper o7-card__image-wrapper o7-card__image-wrapper--rounded o7-card__image-wrapper--with-bg">
+                                    <div class="o7-content-card__image-frame o7-card__image-frame">
+                                        <a href="<?php echo get_permalink($post_id); ?>">
+                                            <?php if ($featured_img): ?>
                                                 <img src="<?php echo esc_url($featured_img); ?>"
                                                     alt="<?php echo esc_attr(get_the_title($post_id)); ?>"
                                                     width="780" height="680"
                                                     loading="lazy" decoding="async"
                                                     class="o7-card__image">
-                                                <?php endif; ?>
-                                            </a>
+                                            <?php endif; ?>
+                                        </a>
 
-                                        </div>
-                                        
-                                        <?php if (!empty($all_tags)) : ?>
-                                            <div class="o7-hover-chip">
-                                                <div class="o7-hover-chip__inner">
-                                                    <?php foreach ($all_tags as $tag): ?>
-                                                        <div class="o7-hover-chip__buton">
-                                                            <?= esc_html($tag); ?>
-                                                        </div>
-                                                    <?php endforeach; ?>
-                                                    <div class="o7-hover-chip__buton o7-hover-chip__buton--hidden-pc">
-                                                        +1
+                                    </div>
+
+                                    <?php if (!empty($all_tags)) : ?>
+                                        <div class="o7-hover-chip">
+                                            <svg class="o7-hover-chip__bg-span-icon-top" aria-hidden="true" focusable="false">
+                                                <use href="<?php echo get_template_directory_uri(); ?>/assets/icons/svg-icon-sprite.svg#chip-radius-2"></use>
+                                            </svg>
+                                            <svg class="o7-hover-chip__bg-span-icon" aria-hidden="true" focusable="false">
+                                                <use href="<?php echo get_template_directory_uri(); ?>/assets/icons/svg-icon-sprite.svg#chip-radius-2"></use>
+                                            </svg>
+                                            <div class="o7-hover-chip__inner">
+                                                <?php foreach ($all_tags as $tag): ?>
+                                                    <div class="o7-hover-chip__buton">
+                                                        <?= esc_html($tag); ?>
                                                     </div>
+                                                <?php endforeach; ?>
+                                                <div class="o7-hover-chip__buton o7-hover-chip__buton--hidden-pc">
+                                                    +1
                                                 </div>
                                             </div>
-                                        <?php endif; ?>
+                                        </div>
+                                    <?php endif; ?>
 
-                                        <?php if ($platform): ?>
+                                    <?php if ($platform): ?>
 
-                                            <div class="o7-hover-icon o7-hover-icon--<?php echo strtolower($platform); ?> o7-hover-icon--bottom-left">
-                                                <div class="o7-hover-icon__bg">
-                                                    <svg class="o7-hover-icon__icon" aria-hidden="true" focusable="false">
-                                                        <use href="<?php echo get_template_directory_uri(); ?>/assets/icons/svg-icon-sprite.svg#card-curve-img-1"></use>
+                                        <div class="o7-hover-icon o7-hover-icon--<?php echo strtolower($platform); ?> o7-hover-icon--bottom-left">
+                                            <div class="o7-hover-icon__bg">
+                                                <svg class="o7-hover-icon__icon" aria-hidden="true" focusable="false">
+                                                    <use href="<?php echo get_template_directory_uri(); ?>/assets/icons/svg-icon-sprite.svg#card-curve-img-1"></use>
+                                                </svg>
+                                                <div class="o7-hover-icon__bg-span-wrapper">
+                                                    <span class="o7-hover-icon__bg-span"></span>
+                                                    <svg class="o7-hover-icon__bg-span-icon" aria-hidden="true"
+                                                        focusable="false">
+                                                        <use href="<?php echo get_template_directory_uri(); ?>/assets/icons/svg-icon-sprite.svg#card-curve-img-2"></use>
                                                     </svg>
-                                                    <div class="o7-hover-icon__bg-span-wrapper">
-                                                        <span class="o7-hover-icon__bg-span"></span>
-                                                        <svg class="o7-hover-icon__bg-span-icon" aria-hidden="true"
-                                                            focusable="false">
-                                                                <use href="<?php echo get_template_directory_uri(); ?>/assets/icons/svg-icon-sprite.svg#card-curve-img-2"></use>
-                                                        </svg>
-                                                    </div>
-                                                </div>
-
-                                                <div class="o7-hover-icon__inner">
-                                                    <img  src="<?php echo get_template_directory_uri(); ?>/assets/images/archive/hover-icon-<?php echo strtolower($platform); ?>.webp"
-                                                        alt="<?php echo esc_attr($platform); ?>" loading="lazy" decoding="async" width="80px"
-                                                        height="80px" />
                                                 </div>
                                             </div>
-                                        <?php endif; ?>
-                                    </div>
 
-                                    <!-- Title & Subtitle -->
-
-                                    <div class="o7-card__info-block">
-                                        <div class="o7-card__title-wrapper">
-                                            <p class="o7-card__title-decorative-dot o7-card__title"><?php echo esc_html($platform); ?></p>
+                                            <div class="o7-hover-icon__inner">
+                                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/archive/hover-icon-<?php echo strtolower($platform); ?>.webp"
+                                                    alt="<?php echo esc_attr($platform); ?>" loading="lazy" decoding="async" width="80px"
+                                                    height="80px" />
+                                            </div>
                                         </div>
+                                    <?php endif; ?>
+                                </div>
 
-                                        <h3 class="o7-content-card__header">
-                                            <a href="<?php the_permalink(); ?>">
-                                                <?php the_title(); ?>
-                                            </a> 
-                                        </h3>
-                                        <?php if ($subtitle): ?>
-                                        <p class="o7-content-card__blog-para">
-                                           <?php echo esc_html($subtitle); ?>
-                                        </p>
-                                        <?php endif; ?>
+                                <!-- Title & Subtitle -->
+
+                                <div class="o7-card__info-block">
+                                    <div class="o7-card__title-wrapper">
+                                        <p class="o7-card__title-decorative-dot o7-card__title"><?php echo esc_html($platform); ?></p>
                                     </div>
-                                
+
+                                    <h3 class="o7-content-card__header">
+                                        <a href="<?php the_permalink(); ?>">
+                                            <?php the_title(); ?>
+                                        </a>
+                                    </h3>
+                                    <?php if ($subtitle): ?>
+                                        <p class="o7-content-card__blog-para">
+                                            <?php echo esc_html($subtitle); ?>
+                                        </p>
+                                    <?php endif; ?>
+                                </div>
+
                             </article>
-                        <?php
+                    <?php
                         endwhile;
                         wp_reset_postdata();
                     endif;
